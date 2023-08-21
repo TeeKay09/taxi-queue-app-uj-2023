a8d5311115e71ec4681287574e0581f486a39a07
takudzwa <takudzwa.ndlovu@gmail.com>
2023-08-21 16:42:06 +0200

    commit

--- public/no-api/taxi-queue.no-api.js
+++ public/no-api/taxi-queue.no-api.js
@@ -1,35 +1,59 @@
-document.addEventListener('alpine:init', () => {
-
-	Alpine.data('TaxiQueue', () => {
-
-		return {
-			version: 'no-api-1.0',
-			joinQueue() {
-
-			},
-			leaveQueue() {
-
-			},
-
-			joinTaxiQueue() {
-
-			},
-
-			queueLength() {
-
-			},
-
-			taxiQueueLength() {
-
-			},
-
-			taxiDepart() {
-
-			}
-		}
-
+document.addEventListener("alpine:init", () => {
+	Alpine.data("TaxiQueue", () => {
+	  return {
+		version: "no-api-1.0",
+		taxi_queue_count: 0,
+		queueLengthPassnger: 0,
+		queueLengthPassenger: 0,
+		queueLengthTaxi: 0,
+		taxiDepartPassenger: "",
+		TaxiDepartTaxi: "",
+		PassengerTaxiReduction: "",
+  
+		joinQueue() {
+		  this.queueLengthPassnger += 1;
+		  (localStorage = localStorage["queueLengthPassnger"]),
+			this.passengerMessage;
+		},
+		leaveQueue() {
+		  if (this.queueLengthPassnger > 0) {
+			this.queueLengthPassnger -= 1;
+		  }
+		},
+  
+		joinTaxiQueue() {
+		  if (this.queueLengthPassnger >= 12) {
+			this.taxi_queue_count += 1;
+			this.PassengerTaxiReduction = this.queueLengthPassenger - 12;
+			this.queueLengthPassenger = this.PassengerTaxiReduction;
+		  } else {
+			return "Taxi is not full";
+		  }
+		},
+  
+		queueLength() {
+		  if (queueLengthPassnger == 12) {
+			return joinTaxiQueue();
+		  }
+		},
+  
+		taxiQueueLength() {
+		  if (taxi_queue_count == 12) {
+			return joinTaxiQueue();
+		  }
+		},
+  
+		taxiDepart() {
+		  if (this.queueLengthPassnger >= 12) {
+			this.taxiDepartPassenger = this.queueLengthPassnger - 12;
+			this.TaxiDepartTaxi = this.taxi_queue_count - 1;
+			this.queueLengthPassnger = this.taxiDepartPassenger;
+			this.taxi_queue_count = this.TaxiDepartTaxi;
+		  } else {
+			return "Not enough passengers for taxi to depart";
+		  }
+		},
+	  };
 	});
-
-});
-
-
+  });
+  
